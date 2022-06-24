9cb153209431ecbd852d4dc71703ebfa38c9848f
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-06-24 12:13:07 -0700

    pushing up to tst funtuality of test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,12 +3,32 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+const mockUser = {
+  firstName: 'Jane',
+  lastName: 'Doe',
+  email: 'janeD@example.com',
+  password: '12345',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+
+  const agent = request.agent(app);
+  // agent allows us to store cookies between requests
+
+  const user = await UserService.create({ ...mockUser, ...userProps });
+
+  const {email} = user;
+  await (await agent.post('/api/v1/users/session')).setEncoding({email, password});
+return [agent, user];
+};
+
+describe('authentication routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
   it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+    expect(1).toEqual(1);  
   });
   afterAll(() => {
     pool.end();
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,12 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS secrets;
+
+
+CREATE TABLE secrets (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    description TEXT NOT NULL,
+   created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+   );
