b41dec394bd400c84f967b8b39a9d3576c2e9eac
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-06-24 16:05:47 -0700

    my test is failing but I think the issue is in my data set up. I am looking at the demo and maybe I am trying to test the wronghinking this. I cant test the creating a new entry because there is no user attached. So I need to finsih that first

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,35 +2,54 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+// const UserService = require('../lib/services/UserService');
 
 const mockUser = {
-  firstName: 'Jane',
-  lastName: 'Doe',
-  email: 'janeD@example.com',
-  password: '12345',
+  title: 'Scary Story',
+  description:
+    'A bedtime story abount a cannibal who tricked people into eating humans',
+  created_at: ' 0000',
 };
 
-const registerAndLogin = async (userProps = {}) => {
-  const password = userProps.password ?? mockUser.password;
+// const registerAndLogin = async (userProps = {}) => {
+//   const password = userProps.password ?? mockUser.password;
 
-  const agent = request.agent(app);
-  // agent allows us to store cookies between requests
+//   const agent = request.agent(app);
+//   // agent allows us to store cookies between requests
 
-  const user = await UserService.create({ ...mockUser, ...userProps });
+//   const user = await UserService.create({ ...mockUser, ...userProps });
 
-  const {email} = user;
-  await (await agent.post('/api/v1/users/session')).setEncoding({email, password});
-return [agent, user];
-};
+//   const { email } = user;
+
+//   await agent.post('/api/v1/users/session').send({ email, password });
+//   return [agent, user];
+// };
 
 describe('authentication routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);  
+  it('creates a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    console.log(res.body);
+    const { title, description, created_at } = mockUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      title,
+      description,
+      created_at,
+    });
   });
   afterAll(() => {
     pool.end();
   });
+  // it('signs in an existing user', async () => {
+  //   await request(app).post('/api/v1/users').send(mockUser);
+  //   const res = await request(app)
+  //     .post('/api/v1/users/sessions')
+  //     .send({ email: 'janeD@example.com', password: '12345' });
+  //   console.log(res.status);
+  //   expect(res.status).toEqual(200);
+  // });
 });
