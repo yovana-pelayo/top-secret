7f6749304a1df3a0ad5bb502f97fe657eab9532c
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-06-27 23:37:43 -0700

    successfully created a userss table, I could not find the table in beekeper for the life of me then I switched connections and found all of the previous tables

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,12 +1,19 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS secrets;
+-- DROP TABLE IF EXISTS secrets;
+DROP TABLE IF EXISTS userss;
 
-
-CREATE TABLE secrets (
+-- CREATE TABLE secrets (
+--     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+--     title TEXT NOT NULL,
+--     last_name TEXT NOT NULL,
+--     description TEXT NOT NULL,
+--     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+--     );
+CREATE TABLE userss (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    title TEXT NOT NULL,
+    first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
-    description TEXT NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    email TEXT NOT NULL,
+    password_hash TEXT NOT NULL
     );
