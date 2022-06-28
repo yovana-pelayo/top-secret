3e6d7d0f64e38b82b905cbf16408b94e817cf360
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-06-28 01:48:34 -0700

    created secrets file, will getAll function and last test to pass on a hack day

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,15 +1,14 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
--- DROP TABLE IF EXISTS secrets;
+DROP TABLE IF EXISTS secrets;
 DROP TABLE IF EXISTS userss;
 
--- CREATE TABLE secrets (
---     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
---     title TEXT NOT NULL,
---     last_name TEXT NOT NULL,
---     description TEXT NOT NULL,
---     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
---     );
+CREATE TABLE secrets (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    );
 CREATE TABLE userss (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     first_name TEXT NOT NULL,
